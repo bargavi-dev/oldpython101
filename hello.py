8bdd7e550f6c4565a839c7f8c201f91ec595ac6f
Bargavi Kanneganti <bargavi.tech@gmail.com>
2020-08-18 15:26:23 -0500

    modified hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello")
+
+username = input('Please fill in the blanks below:' +
+'(username)' + "'s" + ' favorite subject in school is (subject).')
+
+username = input('What is the name?')
+print(username)
+subject = input('What is the subject?')
+print(subject)
+print(username + "'s favorite subject in school is " + subject + ".")
+
+
